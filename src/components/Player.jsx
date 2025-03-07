cb1fc5f59dd440243e962f427ce3f5a8153bd48f
mehedi <mehedi25122003@gmail.com>
2025-03-07 15:27:11 +0600

    card section added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { FaUser } from "react-icons/fa";
+import { CiFlag1 } from "react-icons/ci";
 
 const Player = ({player}) => {
-    const {name,price,image,country} = player;
+    const {name,price,image,country,category} = player;
     return (
         <div>
             <img src={image} alt="" />
-            <h1 className='flex gap-2 py-2'><FaUser></FaUser>{name}</h1>
-            <p>{country}</p>
-            <p>Price:{price}BDT</p>
+            <h1 className='flex gap-2 py-2 items-center'><FaUser></FaUser>{name}</h1>
+            <div className='flex justify-between items-center border-b-2'>
+            <p className='flex gap-2 py-2 items-center'><CiFlag1></CiFlag1>{country}</p>
+            <p>{category}</p>
+            </div>
+            <p>Price:${price}</p>
         </div>
     );
 };
