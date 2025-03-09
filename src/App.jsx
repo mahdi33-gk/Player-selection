68d6665cf0827535cdf79287a98e4d77ac467184
mehedi <mehedi25122003@gmail.com>
2025-03-09 19:33:05 +0600

    not workinng againn

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,50 @@
-
-import { useState } from 'react'
-import './App.css'
-import Beforefooter from './components/Beforefooter'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Players from './components/Players'
-import Plaayeradded from './components/Plaayeradded'
+import { useState } from "react";
+import "./App.css";
+import Beforefooter from "./components/Beforefooter";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Players from "./components/Players";
+import Plaayeradded from "./components/Plaayeradded";
 
 function App() {
-  const [money, setMoney] = useState(0)
-  const moneyHandler = ()=>{
+  const [money, setMoney] = useState(0);
+  const moneyHandler = () => {
     const newMony = money + 4000000;
-    setMoney(newMony)
-    setBalance(newMony)
-  }
-  const [players1, setPlayers] = useState(0)
+    setMoney(newMony);
+    setBalance(newMony);
+  };
+  const [players1, setPlayers] = useState(0);
 
-  const [selectplayer, setSelectplayer] = useState([])
-  const [balance, setBalance] = useState(money)
-  const addPlayer = (player) =>{
-    const newSelection = [...selectplayer, player]
-    const newBalance = balance - player.price
-    setBalance(newBalance)
-    setMoney(newBalance)
-    setSelectplayer(newSelection)
-    
-    const newPlayer = players1 + 1;
-    setPlayers(newPlayer)
-  }
+  const [selectplayer, setSelectplayer] = useState([]);
+  const [balance, setBalance] = useState(money);
+  const addPlayer = (player) => {
+    if (!balance > player.price) {
+      return alert("not enough money");
+    } else {
+      const newSelection = [...selectplayer, player];
+      const newBalance = balance - player.price;
+      setBalance(newBalance);
+      setMoney(newBalance);
+      setSelectplayer(newSelection);
+
+      const newPlayer = players1 + 1;
+      setPlayers(newPlayer);
+    }
+  };
 
   return (
     <>
-      <main className='w-10/12 mx-auto'>
-      <Header money={money}></Header>
-      <Hero moneyHandler={moneyHandler}></Hero>
-      <Players players1={players1} addPlayer={addPlayer}></Players>
-      <Plaayeradded selectplayer={selectplayer}></Plaayeradded>
-      
+      <main className="w-10/12 mx-auto">
+        <Header money={money}></Header>
+        <Hero moneyHandler={moneyHandler}></Hero>
+        <Players players1={players1} addPlayer={addPlayer}></Players>
+        <Plaayeradded selectplayer={selectplayer}></Plaayeradded>
       </main>
       <Footer></Footer>
       <Beforefooter></Beforefooter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
